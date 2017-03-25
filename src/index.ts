8daabd74f698930a4c84ae4e8202bacd8d3ee79a
James-Frowen Laptop2 <jamesfrowen@talktalk.net>
2017-03-25 21:10:00 +0000

    playing around with parsers to find class

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import * as request from 'request';
 import * as htmlparser from 'htmlparser';
+import * as fs from 'fs';
+import * as jsonQuery from 'json-query';
 
 var gamelist: string[] = require("./gamelist/games.json");
 
 
-function getGamePage(gameName: string) {
+function getGamePage(gameName: string): void {
     var url = encodeURI("https://www.google.co.uk/search?q=" + gameName + " site:store.steampowered.com&btnI");
     request(url, {}, (e, r, body) => {
         var options = {
@@ -13,18 +15,47 @@ function getGamePage(gameName: string) {
                 removeScript: true //Remove scripts for innerHTML and outerHTML output
             }
         };
-        var handler = new htmlparser.DefaultHandler(function (error, dom) {
-            if (error) {
-
-            }
-            else {
-
-            }
-        });
+        var handler = getHtmlJsonHandler();
         var parser = new htmlparser.Parser(handler);
-        parser
+        parser.parseComplete(body);
     });
 }
 
+function getHtmlJsonHandler() {
+    return new htmlparser.DefaultHandler(function (error, dom) {
+        if (error) { }
+        else {
+            var json = JSON.stringify(dom);
+            fs.writeFileSync("dump/dom.json", json);
+            searchJson(json);
+        }
+    });
+}
+function searchJson(json: any): void {
+    var helper = {
+        searchForClass: (input:any, className:string):any => {
+            //if attribs contains class add to return list
+            //search children
+            // return array of classes
+        }
+    };
+    var testJson = [
+        {
+            children: [
+                { children: [{ "name": "one", "data": "123" }, { "name": "two", "data": "234" }] }
+            ]
+        }];
+    var pattern = "[**][*name=one].name";
+    //var pattern = `[**][*.name=body].name`;
+    var out = jsonQuery(pattern, { data: testJson });
+    console.log(out.value);
 
-getGamePage(gamelist[0]);
+}
+
+var tagsOnPage = "glance_tags_ctn popular_tags_ctn";
+
+//getGamePage(gamelist[0]);
+
+var json = require("./dump/dom.json");
+
+searchJson(json);
